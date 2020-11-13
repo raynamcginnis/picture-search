3f3b83c63ca1030518de7696a6ac55b7fe0d93cf
Rayna McGinnis <raynamcginnis@Raynas-MacBook-Pro.local>
2020-11-13 15:39:41 -0700

    initial deploy

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import SearchBar from './SearchBar';
+
+const App = () => {
+    return <div className = "ui container app-container">
+        <SearchBar />
+    </div>
+}
+
+export default App;
+
